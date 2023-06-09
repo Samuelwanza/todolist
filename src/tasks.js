a59f58580981031c5fb626b8288def880c94c725
Samuel wanza <s.wanza@alustudent.com>
2023-06-10 00:24:03 +0200

    setup the todolist project, implemented the webpack, the todolist in separate modules, included template

---
+++ src/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    index: 1,
+    description: "Studying redux state management",
+    completed: true,
+  },
+  {
+    index: 2,
+    description: "Designing a database for a personal project",
+    completed: false,
+  },
+  {
+    index: 3,
+    description: "Setting the system architecture for personal project",
+    completed: true,
+  },
+];
